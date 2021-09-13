f15a197c982e8c9e508c6764a6cb108216e86412
TenViki <viktorkomarek28@gmail.com>
2021-09-13 17:15:44 +0200

    Adds logget

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import config from "config";
+import { Client, Intents } from "discord.js";
+import * as logger from "./utils/logger";
+
+// Starting the logger
+logger.setup();
+
+// Creating the bot object with intents
+const bot: Client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGES,
+  ],
+});
+
+// Loging the bot into discord system
+bot.login(config.get("bot-token"));
+
+bot.once("ready", (cl) => {
+  logger.log(
+    `Successfuly logged into discord system as ${cl.user.username}#${cl.user.discriminator}`
+  );
+});
